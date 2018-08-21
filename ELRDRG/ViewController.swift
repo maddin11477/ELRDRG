dd01fcdece316c021123639a1c5f3589b993943b
Martin Mangold <Martin@martins-imac-4.local>
2018-08-21 16:55:18 +0200

    Initial Commit

---
+++ ELRDRG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ELRDRG
+//
+//  Created by Martin Mangold on 21.08.18.
+//  Copyright © 2018 Martin Mangold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
