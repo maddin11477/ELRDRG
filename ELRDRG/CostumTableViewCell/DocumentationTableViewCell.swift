9e12ad524387f9db8f2bc4cbb9b8245963be5ffe
Martin Mangold <Martin@martins-imac-5.fritz.box>
2018-09-02 23:05:59 +0200

    missed some data....

---
+++ ELRDRG/CostumTableViewCell/DocumentationTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DocumentationTableViewCell.swift
+//  ELRDRG
+//
+//  Created by Martin Mangold on 02.09.18.
+//  Copyright © 2018 Martin Mangold. All rights reserved.
+//
+
+import UIKit
+
+class DocumentationTableViewCell: UITableViewCell {
+    @IBOutlet weak var ID: UILabel!
+    @IBOutlet weak var CreationDate: UILabel!
+    @IBOutlet weak var Content: UITextView!
+    @IBOutlet weak var Thumbnail: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
